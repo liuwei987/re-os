5fe5ccd94291dc61ded6d422aae7d131b5e7dcf8
qsort987 <prj_share@sina.com>
2021-06-27 09:09:21 +0800

    cpu: add get_cpuid function for CPU module
    Signed-off-by: qsort987 <prj_share@sina.com>

---
+++ arch/x86/kernel/cpu/cpu.c
@@ -0,0 +1,50 @@
+#include "cpu.h"
+#include "printk.h"
+
+#define SUPPORT_CPUID_FLAG (1 << 21)
+
+static int is_support_cpuid(void)
+{
+	long ret = 0;
+	/* sbbl -> dest - (src + cf) */
+	__asm__ volatile ("mov $0, %%rax\n" \
+			"not %%rax\n" \
+			"add $1, %%rax\n" \
+			"clc\n" \
+			"pushfq \n" \
+			"pop %%rax\n" \
+			"bts  %1, %%eax\n" \
+			"sbb %0, %0\n" \
+			:"=r"(ret) \
+			:"r"(SUPPORT_CPUID_FLAG) \
+			:"cc", "memory");
+	/* return true if bit offset in EFLAGS was clear */
+	return (ret == 0);
+}
+
+int get_cpuid(int *eax, int *ebx, int *ecx, int *edx, int leaf, int subleaf)
+{
+
+	/* not support CPUID, printk and return */
+	if (!is_support_cpuid()) {
+		color_printk(WHITE, BLACK, "The processor does not supporte CPUID instruction!\n");
+		return -1;
+	}
+
+	__asm__ volatile (\
+			"cpuid \n\r"\
+			:"=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)\
+			:"a" (leaf), "c" (subleaf)\
+			:"memory");
+}
+
+static int feature_supported(void)
+{
+
+	int i, bits = 32;
+	int eax, ebx, ecx, edx;
+	int leaf = CPUID_EAX_FEATURE, subleaf = 0;
+
+	get_cpuid(&eax, &ebx, &ecx, &edx, leaf, subleaf);
+	if (edx & )
+}
