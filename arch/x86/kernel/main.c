0814737f39d4e7fe22276fa50cb400217aab83fc
qsort987 <prj_share@sina.com>
2021-06-27 09:09:21 +0800

    cpu: add get_cpuid function for CPU module
    Signed-off-by: qsort987 <prj_share@sina.com>

--- arch/x86/kernel/main.c
+++ arch/x86/kernel/main.c
@@ -1,11 +1,18 @@
 #include "printk.h"
+#include "cpu.h"
 
 void start_kernel(void)
 {
+
+	int eax, ebx, ecx, edx, leaf, subleaf;
 
 	/* TODO: init_print should be initaionazed before kernel start */
 	init_printk();
 	color_printk(WHITE, BLACK, "Hello world!\n");
 
+	leaf = CPUID_EAX_CPU;
+	subleaf = 0;
+	get_cpuid(&eax, &ebx, &ecx, &edx, leaf, subleaf);
+	color_printk(GREEN, BLACK, "CPU NAME: %s%s%s\n", &ebx, &ecx, &edx);
 	while (1);
 }
