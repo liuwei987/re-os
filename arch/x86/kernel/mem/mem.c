26f251d89937b57cdb837ea990f157fd16b5bd22
qsort987 <prj_share@sina.com>
2021-08-08 13:21:33 +0800

    mem: caculate the available physical address memory
    1. refine the E820 struct and define a global memory struct to manage
    E820.
    2. caculate the total memory and how many pages for 2M per page.
    
    Signed-off-by: qsort987 <prj_share@sina.com>

--- arch/x86/kernel/mem/mem.c
+++ arch/x86/kernel/mem/mem.c
@@ -1,26 +1,34 @@
 #include "mem.h"
 #include "printk.h"
 
+
+struct global_mem_descriptor global_mem_struct = {{0}, 0};
+
 void init_memory()
 {
 
 	unsigned long total_mem = 0;
-	struct memory_e820 *p;
-	unsigned int e820_len = 32, i;
+	struct E820 *p;
 
-	p = (struct memory_e820 *)0xffff800000007e00;
+	unsigned int i, e820_entrys;
+	e820_entrys = sizeof(global_mem_struct.e820) / sizeof(global_mem_struct.e820[0]);
+
+	p = (struct E820 *)0xffff800000007e00;
 
 	/* walk over the e820 table */
-	color_printk(ORANGE, BLACK, "Display Physic Address MAP,Type(1:RAM,2:ROM or Reserved,3:ACPI Reclaim Memory,4:ACPI NVS Memory,Others:Undefine)\n");
-	for (i = 0; i < e820_len; i ++) {
-		color_printk(ORANGE, BLACK, "Address:%#010x,%08x\tLength:%#010x,%08x\tType:%#010x\n", p->addr2, p->addr1, p->length2, p->length1, p->type);
+	//color_printk(ORANGE, BLACK, "Display Physic Address MAP,Type(1:RAM,2:ROM or Reserved,3:ACPI Reclaim Memory,4:ACPI NVS Memory,Others:Undefine)\n");
+	for (i = 0; i < e820_entrys; i ++) {
+		//color_printk(ORANGE, BLACK, "Address:%#010x,%08x\tLength:%#010x,%08x\tType:%#010x\n", p->addr2, p->addr1, p->length2, p->length1, p->type);
+		color_printk(ORANGE, BLACK, "Address:%#010x\tLength:%#010x\tType:%#010x\n", p->address, p->length, p->type);
 
 		if (p->type == 1) {
-			unsigned long tmp = 0;
-			tmp = p->length2;
-			total_mem += p->length1;
-			total_mem += tmp << 32;
+			total_mem += p->length;
 		}
+
+		global_mem_struct.e820[i].address = p->address;
+		global_mem_struct.e820[i].length  = p->length;
+		global_mem_struct.e820[i].type    = p->type;
+		global_mem_struct.e820_length = i;
 
 		if (p->type > 4) {
 			break;
@@ -31,4 +39,22 @@ void init_memory()
 
 	color_printk(ORANGE, BLACK, "OS can use total RAM:%#018lx\n", total_mem);
 
+	total_mem = 0;
+	for (i = 0; i < global_mem_struct.e820_length; i ++) {
+		unsigned long start, end;
+
+		if (global_mem_struct.e820[i].type != 1)
+			continue;
+
+		start = PAGE_2M_ALIGN(global_mem_struct.e820[i].address);
+		//end = PAGE_2M_ALIGN(global_mem_struct.e820[i].address + global_mem_struct.e820[i].length);  don't use
+		end = (global_mem_struct.e820[i].address + global_mem_struct.e820[i].length) & PAGE_2M_MASK;
+		//end = ((global_mem_struct.e820[i].address + global_mem_struct.e820[i].length) >> PAGE_2M_SHIFT) << PAGE_2M_SHIFT;
+		if (end < start)
+			continue;
+
+		total_mem += (end - start) >> PAGE_2M_SHIFT;
+	}
+
+	color_printk(ORANGE, BLACK, "OS can use 2M pages:%#010x=%010d\n", total_mem, total_mem);
 }
--- arch/x86/kernel/mem/mem.h
+++ arch/x86/kernel/mem/mem.h
@@ -1,7 +1,25 @@
 #ifndef __MEM_H__
 #define __MEM_H__
 
-struct memory_e820 {
+#define PTRS_PER_PAGE 512UL
+#define LINEAR_OFFSET ((unsigned long)0xffff800000000000)
+
+//#define PAGE_GDT_SHIFT 39
+#define PAGE_4K_SHIFT		12UL
+#define PAGE_2M_SHIFT		21UL
+#define PAGE_1G_SHIFT		30UL
+
+#define PAGE_4K			(1UL << PAGE_4K_SHIFT)
+#define PAGE_2M			(1UL << PAGE_2M_SHIFT)
+#define PAGE_4K_MASK		(~(PAGE_4K - 1UL))
+#define PAGE_2M_MASK		(~(PAGE_2M - 1UL))
+#define PAGE_4K_ALIGN(x)	((x + PAGE_4K - 1UL) & PAGE_4K_MASK)
+#define PAGE_2M_ALIGN(x)	((x + PAGE_2M - 1UL) & PAGE_2M_MASK)
+
+#define virt_to_phy(addr)	((unsigned long)addr - LINEAR_OFFSET)
+#define phy_to_virt(addr)	((unsigned long)addr + LINEAR_OFFSET)
+
+struct memory_e820_fmt {
 	unsigned int addr1;
 	unsigned int addr2;
 	unsigned int length1;
@@ -9,6 +27,18 @@ struct memory_e820 {
 	unsigned int type;
 };
 
+struct E820 {
+	unsigned long address;
+	unsigned long length;
+	unsigned int type;
+}__attribute__((packed));
+
+struct global_mem_descriptor {
+	struct E820 e820[32];
+	unsigned long e820_length;
+};
+
 void init_memory();
 
+extern struct global_mem_descriptor global_mem_struct;
 #endif
