bf0cc5ce9012153408fd5b48190a1808a62aaf79
qsort987 <prj_share@sina.com>
2021-08-11 23:55:51 +0800

    memset: add memset function to the lib file
    Signed-off-by: qsort987 <prj_share@sina.com>

--- modules/lib/lib.h
+++ modules/lib/lib.h
@@ -3,6 +3,44 @@
 
 #define NULL 0
 
-int strlen(char *str);
+static inline int strlen(char *str)
+{
+	register int __res;
+	__asm__ __volatile__ ( "cld \n\t"
+				"repne \n\t"
+				"scasb \n\t"
+				"notl %0 \n\t"
+				"decl %0 \n\t"
+				:"=c"(__res)
+				:"D"(str), "a"(0), "0"(0xffffffff)
+			);
+
+	return __res;
+}
+
+
+static inline void * memset(void * Address,unsigned char C,long Count)
+{
+        int d0,d1;
+        unsigned long tmp = C * 0x0101010101010101UL;
+        __asm__ __volatile__    (       "cld    \n\t"
+                                        "rep    \n\t"
+                                        "stosq  \n\t"
+                                        "testb  $4, %b3 \n\t"
+                                        "je     1f      \n\t"
+                                        "stosl  \n\t"
+                                        "1:\ttestb      $2, %b3 \n\t"
+                                        "je     2f\n\t"
+                                        "stosw  \n\t"
+                                        "2:\ttestb      $1, %b3 \n\t"
+                                        "je     3f      \n\t"
+                                        "stosb  \n\t"
+                                        "3:     \n\t"
+                                        :"=&c"(d0),"=&D"(d1)
+                                        :"a"(tmp),"q"(Count),"0"(Count/8),"1"(Address)
+                                        :"memory"
+                                );
+        return Address;
+}
 
 #endif
