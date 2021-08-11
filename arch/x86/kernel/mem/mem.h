7baa4b61f68bc91dfb213565c76bb822c8c1a015
qsort987 <prj_share@sina.com>
2021-08-11 23:57:14 +0800

    mem.c: init the struction of bits_map, page_struct, zone_struct
    Signed-off-by: qsort987 <prj_share@sina.com>

--- arch/x86/kernel/mem/mem.h
+++ arch/x86/kernel/mem/mem.h
@@ -21,7 +21,24 @@
 #define virt_to_phy(addr)	((unsigned long)addr - LINEAR_OFFSET)
 #define phy_to_virt(addr)	((unsigned long)addr + LINEAR_OFFSET)
 
-#define flash_tlp()					\
+/* alloc_pages zone select*/
+#define ZONE_DMA		(1UL << 0UL)
+#define ZONE_NORMAL		(1UL << 1UL)
+#define ZONE_UNMAPED		(1UL << 2UL)
+
+/* page's struct attribute (alloc_pages flag) */
+#define PG_PTable_Mapped	(1UL << 0UL)
+#define PG_Kernel_Init		(1UL << 1UL)
+#define PG_Referenced		(1UL << 2UL)
+#define PG_Dirty		(1UL << 3UL)
+#define PG_Active		(1UL << 4UL)
+#define PG_Up_To_Date		(1UL << 5UL)
+#define PG_Device		(1UL << 6UL)
+#define PG_Kernel		(1UL << 7UL)
+#define PG_K_Share_To_U		(1UL << 8UL)
+#define PG_Slab			(1UL << 9UL)
+
+#define flush_tlb()					\
 do {							\
 	unsigned long *tmpreg;				\
 	__asm__ __volatile__("movq %%cr3, %0\n\t"	\
@@ -30,7 +47,7 @@ do {							\
 				::"memory");		\
 } while(0)
 
-inline unsigned long *get_gdt()
+static inline unsigned long *get_gdt()
 {	
 	unsigned long *gdt;
 	__asm__ __volatile__("movq %%cr3, %0\n\t"
@@ -101,6 +118,7 @@ struct Zone {
 
 void init_memory();
 
+extern unsigned long *global_cr3;
 extern struct global_mem_descriptor global_mem_struct;
-unsigned int ZONE_NORMAL_INDEX = ZONE_DMA_INDEX ZONE_UNMAPED_INDEX = 0;
+extern unsigned int ZONE_NORMAL_INDEX, ZONE_DMA_INDEX, ZONE_UNMAPED_INDEX;
 #endif
